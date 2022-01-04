abdb7da11ca68e198a950a5ef3773d8e4f418794
dave <hunter>
2022-01-05 08:39:06 +1100

    changed to uniqidReal()

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 session_start();
 include('database.inc.php'); //database connection
 require_once('config.inc.php'); //reddit and hcaptcha config
+function uniqidReal($lenght = 13) {
+        // uniqid gives 13 chars, but you could adjust it to your needs.
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($lenght / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+        } else {
+            throw new Exception("no cryptographically secure random function available");
+        }
+        return substr(bin2hex($bytes), 0, $lenght);
+    }
 
 
 function getToken($length){
